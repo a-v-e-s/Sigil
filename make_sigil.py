1792ce83dec4fdf7de61cab28cdca3c669900622
Jon David Tannehill <jdtannehill2490@gmail.com>
2019-06-11 11:37:06 -0500

    created new keybinding: control-n

--- make_sigil.py
+++ make_sigil.py
@@ -4,10 +4,17 @@ make_sigil.py:
 Author: Jon David Tannehill
 """
 
-from PIL import Image, ImageFont, ImageDraw
+from PIL import Image, ImageFont, ImageDraw, ImageOps
 import tkinter as tk
 import subprocess, sys, random, math
 
+"""
+PIL
+"Effects": Reduce Noice, Spread, Edge Detect, 
+"FX": Swirl, Implode, Oil Paint
+Add better colorization?
+Fix block/position issue
+"""
 
 class Gui():
     def __init__(self):
@@ -19,43 +26,55 @@ class Gui():
         self.entries = []
         self.chop_width = tk.IntVar()
         self.chop_height = tk.IntVar()
+        self.colorized = tk.IntVar()
         a = tk.Button(self.root)
         b = tk.Button(self.root)
         c = tk.Label(self.root)
-        d = tk.Label(self.root)
-        e = tk.Scale(self.root)        
+        d = tk.Scale(self.root)
+        e = tk.Label(self.root)        
         f = tk.Scale(self.root)
-        g = tk.Button(self.root)
-        self.widgets = [a, b, c, d, e, f, g]
+        g = tk.Label(self.root)
+        h = tk.Checkbutton(self.root)
+        z = tk.Button(self.root)
+        self.widgets = [a, b, c, d, e, f, g, h, z]
         a.configure(text='Add Another', command=self.add_entry)
         a.grid(row=self.rownum-4, column=1)
         b.configure(text='Delete Previous', command=self.remove)
         b.grid(row=self.rownum-4, column=2)
         c.configure(text='Chop width:')
         c.grid(row=self.rownum-3, column=1)
-        d.configure(text='Chop height:')
+        d.configure(from_=8, to=16, variable=self.chop_width, orient='horizontal')
         d.grid(row=self.rownum-3, column=2)
-        e.configure(from_=5, to=20, variable=self.chop_width, orient='horizontal')
+        e.configure(text='Chop height:')
         e.grid(row=self.rownum-2, column=1)
-        f.configure(from_=5, to=20, variable=self.chop_height, orient='horizontal')
+        f.configure(from_=8, to=16, variable=self.chop_height, orient='horizontal')
         f.grid(row=self.rownum-2, column=2)
-        g.configure(text='Go!', command=self.make_sigil)
-        g.grid(row=self.rownum-1, column=1, columnspan=2)
+        g.configure(text='Colorize:')
+        g.grid(row=self.rownum-1, column=1)
+        h.configure(variable=self.colorized, offvalue=0, onvalue=1)
+        h.grid(row=self.rownum-1, column=2)
+        z.configure(text='Go!', command=self.make_sigil)
+        z.grid(row=self.rownum, column=1, columnspan=2)
 
         self.add_entry()
         self.add_entry()
+        d.set(10)
+        f.set(10)
+        h.deselect()
 
         self.font_options = [x for x in subprocess.getoutput('ls fonts').split() if x.endswith('tf')]
         self.chop_width.set(10)
         self.chop_height.set(10)
         self.entries[0].focus_set()
         
-        #self.root.bind(sequence='<Return>', func=self.make_sigil)
+        self.root.bind(sequence='<Return>', func=(lambda x=self.make_sigil: self.make_sigil()))
+        self.root.bind(sequence='<Control-KeyPress-n>', func=(lambda y=self.add_entry: self.add_entry()))
+        self.root.bind(sequence='<Control-KeyPress-d>', func=(lambda z=self.remove: self.remove()))
 
 
     def add_entry(self):
         text = tk.StringVar()
-        entry = tk.Entry(self.root, width=50, textvariable=text)
+        entry = tk.Entry(self.root, width=40, textvariable=text)
         entry.grid(row=self.rownum-6, column=1, columnspan=2)
         self.phrases.append(text)
         self.entries.append(entry)
@@ -66,7 +85,9 @@ class Gui():
         self.widgets[3].grid(row=self.rownum-3, column=2)
         self.widgets[4].grid(row=self.rownum-2, column=1)
         self.widgets[5].grid(row=self.rownum-2, column=2)
-        self.widgets[6].grid(row=self.rownum-1, column=1, columnspan=2)
+        self.widgets[6].grid(row=self.rownum-1, column=1)
+        self.widgets[7].grid(row=self.rownum-1, column=2)
+        self.widgets[8].grid(row=self.rownum, column=1, columnspan=2)
 
     
     def remove(self):
@@ -81,17 +102,28 @@ class Gui():
         self.widgets[3].grid(row=self.rownum-3, column=2)
         self.widgets[4].grid(row=self.rownum-2, column=1)
         self.widgets[5].grid(row=self.rownum-2, column=2)
-        self.widgets[6].grid(row=self.rownum-1, column=1, columnspan=2)
+        self.widgets[6].grid(row=self.rownum-1, column=1)
+        self.widgets[7].grid(row=self.rownum-1, column=2)
+        self.widgets[8].grid(row=self.rownum, column=1, columnspan=2)
         
 
     def make_sigil(self):
         for a in self.phrases:
-            font = ImageFont.truetype(font=random.choice(self.font_options), size=32, encoding='unic')
+            font = ImageFont.truetype(font=random.choice(self.font_options), size=48, encoding='unic')
             text = a.get()
             text_width, text_height = font.getsize(text)
-            canvas = Image.new('RGB', (text_width, text_height), "white")
+            canvas = Image.new('L', (text_width, text_height), "white")
             draw = ImageDraw.Draw(canvas)
             draw.text((0, 0), text, 'black', font)
+
+            if self.colorized.get():
+                text_color = (random.choice(range(255)), random.choice(range(255)), random.choice(range(255)))
+                background_color = (random.choice(range(255)), random.choice(range(255)), random.choice(range(255)))
+                mid = (random.choice(range(255)), random.choice(range(255)), random.choice(range(255)))
+                midpoint = random.choice(range(255))
+                blackpoint = random.choice(range(0, midpoint))
+                whitepoint = random.choice(range(midpoint, 255))
+                canvas = ImageOps.colorize(canvas, text_color, background_color, mid, blackpoint, whitepoint, midpoint)
     
             img_width, img_height = canvas.size
             blocks = []
@@ -102,14 +134,14 @@ class Gui():
             
             sigil_width = math.ceil(math.sqrt(len(blocks))) * self.chop_width.get()
             sigil_height = math.ceil(math.sqrt(len(blocks))) * self.chop_height.get()
-            x, y = 0, (sigil_height - self.chop_height.get())
+            x, y = 0, sigil_height
             position = (x, y)
             random.shuffle(blocks)
             sigil = Image.new('RGB', (sigil_width, sigil_height), 'white')
 
             for b in blocks:
                 sigil.paste(b, box=position)
-                if (position[0] + self.chop_width.get()) < sigil_width:
+                if (position[0] + self.chop_width.get()) <= sigil_width:
                     x += self.chop_width.get()
                 else:
                     x = 0
